37d35b99e8bba86d2eec8151785f539a19f5ecfc
rohith996 <dachavaram.rohithkumar@gmail.com>
2020-06-18 09:39:44 -0400

    XML designs added

--- app/src/main/java/com/example/finalprojectgroup8/MainActivity.java
+++ app/src/main/java/com/example/finalprojectgroup8/MainActivity.java
@@ -6,22 +6,48 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.View;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    private static int time_out = 3000;
+    private static int SPLASH_SCREEN=4000;
+    //variables
+    Animation topAnim,bottomAnim;
+    ImageView image;
+    TextView logo,title;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
+
+        //Animations
+        topAnim = AnimationUtils.loadAnimation(this,R.anim.top_animation);
+        bottomAnim = AnimationUtils.loadAnimation(this,R.anim.bottom_animation);
+        image = findViewById(R.id.imageView2);
+        logo = findViewById(R.id.logo);
+        title = findViewById(R.id.slogan);
+
+        image.setAnimation(topAnim);
+        logo.setAnimation(bottomAnim);
+        title.setAnimation(bottomAnim);
 
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent i = new Intent(MainActivity.this,Login.class);
-                startActivity(i);
+                Intent intent = new Intent(MainActivity.this,Login.class);
+                startActivity(intent);
                 finish();
+
             }
-        },time_out);
+        },SPLASH_SCREEN);
+
+
+
+
     }
 }
